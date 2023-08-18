37e9e9b6ca0c752ab55f03d04c44b65bb4f2e127
Olga Dobrova <dobrova_o_g@mail.ru>
2023-08-18 23:12:01 +0300

    задание к уроку 1 выполнено

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+const title = "Это заголовок";
+const screens = `<img src="../image/screen.png">`;
+const screenPrice = `<img src="../image/screen.png">`;
+const rollback = "не знаю, что такое откат. В каком смысле применять?";
+let fullPrice = "320руб";
+let adaptive = "768px";
+alert(title);
+console.log(rollback);
