0b1057802ed5ddb69e737992d1e0d4ed2fec5e70
Olga Dobrova <dobrova_o_g@mail.ru>
2023-09-23 18:00:45 +0300

    Классы, наследование, вызов метода класса родителя

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 "use strict";
+//Это Урок №16 обязательное задание
+
+class First {
+  constructor() {} // это метод, в него складываем св-ва класса
+  hello() {
+    console.log("Привет, я метод родителя!");
+  }
+}
+
+class Second extends First {
+  //класс Second наследуеся от класса First
+  hello() {
+    super.hello(); //вызывает метод родительского класса
+    console.log("А я - наследуемый метод!");
+  }
+}
+
+const second = new Second(); // новый объект класса Second
+
+second.hello(); // вызов метода объекта
