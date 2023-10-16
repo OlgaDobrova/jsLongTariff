b90915d19971fc847847d1b319d0171019f38f07
Olga Dobrova <dobrova_o_g@mail.ru>
2023-10-16 15:46:15 +0300

    Асинхронность. getData и  sendData - прием и отправка данных

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 "use strict";
+
+const getData = (file) => {
+  return fetch(file).then((response) => response.json());
+};
+
+const sendData = ({ url, data = {}, method = "GET" }) => {
+  return fetch(url, {
+    method: method,
+    body: data,
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  }).then((response) => response.json());
+};
+
+getData("db.json")
+  .then((user) => {
+    sendData({
+      url: "https://jsonplaceholder.typicode.com/posts",
+      data: JSON.stringify(user),
+      method: "POST",
+    }).then((user) => console.log(user));
+  })
+  .catch((error) => console.log(error));
